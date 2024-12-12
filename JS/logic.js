30ca3618b21d10f436cbbd3980cdfb5aaa9b270e
AnLaxina <anilovlaxina@gmail.com>
2024-12-12 14:18:56 -0800

    Refactor for loop to represent accurate grid values

--- JS/logic.js
+++ JS/logic.js
@@ -3,23 +3,23 @@ const sketchPad = document.querySelector(".sketch-pad");
 
 
 // As a test, let's see if adding 16 paragraphs actually works with a for loop
-for (let row = 0; row < 16; row++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
+for (let col = 0; col < 16; col++) {
+    const col = document.createElement("div");
+    col.classList.add("col");
 
     const square = document.createElement("div");
     square.classList.add("square");
 
-    sketchPad.appendChild(row);
-    for (let col = 0; col < 16; col++) {
-        const col = document.createElement("div");
-        col.classList.add("col");
+    sketchPad.appendChild(col);
+    for (let row = 0; row < 16; row++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
 
         const square = document.createElement("div");
         square.classList.add("square");
-        col.appendChild(square);
+        row.appendChild(square);
 
-        row.appendChild(col);
+        col.appendChild(row);
     }
 
 
