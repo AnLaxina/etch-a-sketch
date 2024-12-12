ed646fdcfe7c8ae58d0a0c78968066cf16abdf13
AnLaxina <anilovlaxina@gmail.com>
2024-12-12 14:07:53 -0800

    Add very basic working functionality of adding squares

--- JS/logic.js
+++ JS/logic.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// Add the 16 x 16 grid and see where it goes
+const sketchPad = document.querySelector(".sketch-pad");
+
+
+// As a test, let's see if adding 16 paragraphs actually works with a for loop
+for (let i = 0; i < 16; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    sketchPad.appendChild(square);
+}
