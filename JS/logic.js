6155fc5e90b53be852513ae62b4b879d358dd4d2
AnLaxina <anilovlaxina@gmail.com>
2024-12-12 14:15:55 -0800

    Create basic grid

--- JS/logic.js
+++ JS/logic.js
@@ -3,8 +3,24 @@ const sketchPad = document.querySelector(".sketch-pad");
 
 
 // As a test, let's see if adding 16 paragraphs actually works with a for loop
-for (let i = 0; i < 16; i++) {
+for (let row = 0; row < 16; row++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
     const square = document.createElement("div");
     square.classList.add("square");
-    sketchPad.appendChild(square);
+
+    sketchPad.appendChild(row);
+    for (let col = 0; col < 16; col++) {
+        const col = document.createElement("div");
+        col.classList.add("col");
+
+        const square = document.createElement("div");
+        square.classList.add("square");
+        col.appendChild(square);
+
+        row.appendChild(col);
+    }
+
+
 }
